7e980d13c5c2b7eeae88334e150d4a5175c9a1a9
DmitriyZabava <102691652+DmitriyZabava@users.noreply.github.com>
2022-07-16 08:50:56 +0300

    change destructor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Counter = (props) => {
-  const { value } = props;
+  const { id, value, name } = props;
 
   const formatValue = () => {
     return value === 0 ? "Empty" : value;
@@ -15,23 +15,23 @@ const Counter = (props) => {
 
   return (
     <div>
-      <span>{props.name}</span>
+      <span>{name}</span>
       <span className={getBageClasses()}>{formatValue()}</span>
       <button
         className="btn btn-primary btn-sm m-2"
-        onClick={props.onIncrement.bind(null, props.id)}
+        onClick={props.onIncrement.bind(null, id)}
       >
         +
       </button>
       <button
         className="btn btn-primary btn-sm m-2"
-        onClick={props.onDecrement.bind(null, props.id)}
+        onClick={props.onDecrement.bind(null, id)}
       >
         -
       </button>
       <button
         className="btn btn-danger btn-sm m-2"
-        onClick={props.onDelete.bind(null, props.id)}
+        onClick={props.onDelete.bind(null, id)}
       >
         Delete
       </button>
